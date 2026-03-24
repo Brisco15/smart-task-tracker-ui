456d72146c4632b6c2ecf77ac16f8351a2db31b7
Brisco15 <bbasse0@gmail.com>
2026-03-24 10:58:40 +0100

    feat: refactor auth guard to use Auth service for authentication check

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,11 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = localStorage.getItem('token');
+  const authService = inject(Auth);
 
-  if (!token) {
+
+  if (!authService.isAuthenticated()) {
     router.navigateByUrl('/login');
     return false;
   }
