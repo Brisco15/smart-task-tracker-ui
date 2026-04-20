e9515a1cba84f5c5f98e3fabf77df1bd1629fd44
Brisco15 <bbasse0@gmail.com>
2026-04-20 14:34:38 +0200

    feat: add create task dialog with HTML, CSS, and unit tests

---
+++ src/app/pages/create-task-dialog/create-task-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-task-dialog',
+  imports: [],
+  templateUrl: './create-task-dialog.html',
+  styleUrl: './create-task-dialog.css',
+})
+export class CreateTaskDialog {}
