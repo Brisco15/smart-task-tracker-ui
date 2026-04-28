b6965c592ca42c95e5c1324246c9e1d62e411cc6
Brisco15 <bbasse0@gmail.com>
2026-04-28 13:09:54 +0200

    feat: enhance edit task dialog with form fields and API integration for priorities and statuses

--- src/app/pages/create-task-dialog/create-task-dialog.ts
+++ src/app/pages/create-task-dialog/create-task-dialog.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { TaskDTO } from '../../interfaces/TaskDTO';
--- src/app/pages/edit-task-dialog/edit-task-dialog.ts
+++ src/app/pages/edit-task-dialog/edit-task-dialog.ts
@@ -1,9 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormField, MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { NgIf, NgForOf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { Admin } from '../../services/admin';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Auth } from '../../services/auth';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-task-dialog',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    FormsModule, 
+    NgIf,
+    NgForOf, 
+    CommonModule, 
+    MatButtonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule
+       
+  ],
   templateUrl: './edit-task-dialog.html',
   styleUrl: './edit-task-dialog.css',
 })
-export class EditTaskDialog {}
+export class EditTaskDialog implements OnInit{
+  form: FormGroup;
+  http = inject(HttpClient);
+  priorities: { priorityID: number; priorityName: string}[] = [];
+  statuses: { statusID: number; statusName: string}[] = [];
+  apiStatusUrl = 'http://localhost:5260/api/Status';
+  apiPriorityUrl = 'http://localhost:5260/api/Priorities';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private adminService: Admin,
+    private authService: Auth,
+
+  ){
+    this.form = this.formBuilder.group({
+
+    });
+    this.loadPriorities();
+    this.loadStatuses
+  }
+
+  // Load priorities from the API
+  loadPriorities(){
+    this.http.get<{ priorityID: number; priorityName: string}[]>(this.apiPriorityUrl).subscribe({
+      next: (priorities : { priorityID: number; priorityName: string}[] )=> {
+        this.priorities = priorities;
+      },
+      error: (error: any) => {
+        console.error('Error loading priorities');
+        
+      }
+    })
+  }
+  // load statuses from the API
+  loadStatuses(){
+    this.http.get<{ statusID: number; statusName: string}[]>(this.apiStatusUrl).subscribe({
+      next: ( statuses : { statusID: number; statusName: string}[]) => {
+        this.statuses = statuses;
+      },
+      error: (error: any) => {
+        console.error(' error loading statuses:', error);
+        
+      }
+    })
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
