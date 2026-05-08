2b8844fa387815fb4d647ea1e494b5b999c72e0f
Brisco15 <bbasse0@gmail.com>
2026-05-08 08:26:38 +0200

    feat: configure frontend for Azure backend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+﻿export const environment = {
+  production: true,
+  apiUrl: 'https://smart-task-tracker-api-4114.azurewebsites.net/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+﻿export const environment = {
+  production: false,
+  apiUrl: 'https://smart-task-tracker-api-4114.azurewebsites.net/api'
+};
